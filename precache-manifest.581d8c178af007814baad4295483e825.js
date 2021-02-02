502d174622f1bd145a974f7472c87067aa00a2c3
ElitKondor <elitkondor@gmail.com>
2021-02-02 22:45:12 +0200

    Updates

---
+++ precache-manifest.581d8c178af007814baad4295483e825.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea337cece408907ed92f15a2a6dc6a9b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "c168f7807da9be1dba8d",
+    "url": "/home/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "ab5a855a3f0c0e0fcca1",
+    "url": "/home/static/css/main.aecfe49a.chunk.css"
+  },
+  {
+    "revision": "c168f7807da9be1dba8d",
+    "url": "/home/static/js/2.8c133d70.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/home/static/js/2.8c133d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab5a855a3f0c0e0fcca1",
+    "url": "/home/static/js/main.09f60ebe.chunk.js"
+  },
+  {
+    "revision": "45f1f0382e9cd0bccaf5",
+    "url": "/home/static/js/runtime-main.678fc873.js"
+  },
+  {
+    "revision": "ed52f126fe86e7c787acfe85f50f98e2",
+    "url": "/home/static/media/Main_Photo.ed52f126.jpg"
+  },
+  {
+    "revision": "c509a4c46008514b5a5084d2596c99e4",
+    "url": "/home/static/media/facebook.c509a4c4.svg"
+  },
+  {
+    "revision": "3befe1734b842d3cfc15d2128cbe09c0",
+    "url": "/home/static/media/game-icon.3befe173.jpg"
+  },
+  {
+    "revision": "9429b936c2738b78b6853096051d8b2b",
+    "url": "/home/static/media/iconArt-icon.9429b936.png"
+  },
+  {
+    "revision": "20177402a1e05f0de7322149631cefeb",
+    "url": "/home/static/media/linkedin.20177402.svg"
+  },
+  {
+    "revision": "a888fec0a2859e59674a89bc7722e062",
+    "url": "/home/static/media/telegram.a888fec0.svg"
+  }
+]);
